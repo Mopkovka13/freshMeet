b499f360d4f6f18feee42d6cf328ec0ddbed1535
Mopkovka13 <evgeniyneskin13@gmail.com>
2024-03-28 00:03:35 +0300

    git init

---
+++ src/main/java/com/mopkovka/springboot/freshMeet/FreshMeetApplication.java
@@ -0,0 +1,13 @@
+package com.mopkovka.springboot.freshMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mopkovka/springboot/freshMeet/FreshMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mopkovka.springboot.freshMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
